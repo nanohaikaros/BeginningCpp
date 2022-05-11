dc6f2573581830b30918a5de91d02862baa09dcd
Nanoha <1371689491@qq.com>
2022-05-11 19:10:17 +0800

    1

---
+++ 01/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Testing 1, 2, 3\n";
+    char letter;
+    cout << "Enter a letter to end the program:\n";
+    cin >> letter;
+    return 0;
+}
